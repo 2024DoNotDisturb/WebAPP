67f6a70aaf7d3d74588d38aa12394db47c611a45
kingodjerry <ws.yjkim@gmail.com>
2024-07-12 16:41:43 +0900

    platform homepage update

--- static/js/Platform/home.js
+++ static/js/Platform/home.js
@@ -9,3 +9,16 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var today = new Date();
+    var days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
+    
+    var weekday = days[today.getDay()];
+    var month = today.getMonth() + 1; // getMonth()는 0부터 시작하므로 1을 더합니다.
+    var day = today.getDate();
+    var date = month + '/' + day;
+
+    document.getElementById('weekday').textContent = weekday;
+    document.getElementById('date').textContent = date;
+});
